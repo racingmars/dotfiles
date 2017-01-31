0ee3ff6027d9a8d1a1be06320c748e7b2f9a7da8
Matthew R. Wilson <mwilson@mattwilson.org>
2017-01-30 16:14:44 -0800

    Add automated install script

---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# meant to be installed on a new system, from the user's home directory,
+# with something like:
+# curl -s https://raw.githubusercontent.com/racingmars/dotfiles/master/install \
+#  | sh
+#
+# This whole thing is a pretty poor way to do things but it is quick & dirty.
+
+BACKUP_DIR=${HOME}/dotfiles-backup
+mkdir $BACKUP_DIR
+
+if [ -e lib/dotfiles ]; then
+	echo "Dotfiles directory already exists. Exiting."
+	return
+fi
+
+git clone https://github.com/racingmars/dotfiles.git lib/dotfiles
+
+makelink()
+{
+	SOURCE=$1
+	DEST=$2
+	if [ -e "$DEST" ]; then
+		mv "$DEST" "$BACKUP_DIR"
+	fi
+	ln -s "lib/dotfile/$SOURCE" "$DEST"
+}
+
+makelink zshenv .zshenv
+makelink zshrc .zshrc
+makelink vimrc .vimrc
+makelink Xresources .Xresources
+makelink screenrc .screenrc
+mkdir .vim
+makelink snippets .vim/snippets
+
+# Set up Vim Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+mkdir .ssh
+chmod 700 .ssh
+
+
